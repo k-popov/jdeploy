4764073ad74e834b0566bc46938b3c2144da843c
Kirill Popov <kpopov@griddynamics.com>
2012-06-05 21:40:02 +0400

    Added init.d startup script for the app.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 # Set this to true to redeploy the app (remove its directory first)
 default[:jdeploy][:redeploy] = true
+default[:jdeploy][:java_home] = "/usr"
 default[:jdeploy][:min_java_version] = "1.6.0_31"
 
 default[:jdeploy][:app][:home_dir] = "/opt/application"
@@ -14,3 +15,12 @@ default[:jdeploy][:app][:pre_unpack] = ""
 default[:jdeploy][:app][:post_unpack] = ""
 default[:jdeploy][:app][:pre_start] = ""
 
+# name of the application
+default[:jdeploy][:app][:app_name] = "app"
+# application startup commandline
+# (i.e. "/usr/bin/java node[:jdeploy][:app][:start_cmdline] ")
+default[:jdeploy][:app][:start_cmdline] = '-XX:MaxPermSize=256m -cp "$APP_HOME/*" com.exmple.app.mainClass'
+# application STDOUT redirection file
+default[:jdeploy][:app][:stdout_log] = "/var/log/#{node[:jdeploy][:app][:app_name]}"
+# File to store PID of the application
+default[:jdeploy][:app][:pid_file] = "/var/run/#{node[:jdeploy][:app][:app_name]}"
---
+++ recipes/init.rb
@@ -0,0 +1,11 @@
+template "/etc/init.d/#{node[:jdeploy][:app][:app_name]}" do
+    source "app_init.erb"
+    mode "0755"
+    if params[:options].respond_to?(:has_key?)
+      variables :options => params[:options]
+    end
+end
+
+service "#{node[:jdeploy][:app][:app_name]}" do
+  action [ :enable, :start ]
+end
