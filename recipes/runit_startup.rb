8243017d22b93c519d03191cad142b1d73641fd3
Kirill Popov <kpopov@griddynamics.com>
2012-06-08 14:14:47 +0400

    Fixed a log directory owner.
    It was owned by root after the default "runit_service"
    definition run.

--- recipes/runit_startup.rb
+++ recipes/runit_startup.rb
@@ -3,3 +3,13 @@ include_recipe "runit"
 runit_service "#{node[:jdeploy][:app][:app_name]}" do
     template_name "application"
 end
+
+# fix permissions for the log writer (for genesis user could write logs)
+directory "#{node[:runit][:service_dir]}/#{node[:jdeploy][:app][:app_name]}/log/main" do
+    action :create
+    mode "0755"
+    owner "#{node[:jdeploy][:app][:user]}"
+    group "#{node[:jdeploy][:app][:group]}"
+    recursive true
+end
+
